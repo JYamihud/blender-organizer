d09938edfefc2db25b2a608d3934eb22898c296b
JYamihud <blenderdumbass@gmail.com>
2017-12-29 23:27:08 +0200

    Update renderer.py

--- py_data/renderer.py
+++ py_data/renderer.py
@@ -1,15 +1,26 @@
 import os
-infodata = open(os.getcwd()+"/py_data/renderinfo.data", "r")
+infodata = open(os.getcwd().replace("/py_data", "")+"/py_data/renderinfo.data", "r")
 
 info = infodata.read().split("\n")
 
-blenderpath = info[1]
+
+
 blendfile = info[0]
-startframe = info[2]
-frames = info[3]
+blenderpath = info[1]
+destination = info[2]
+extention = info[3]
+startframe = info[4]
+frames = info[5]
+
+
+
+
+
 
 print blenderpath
 print blendfile
+print destination
+print extention
 print startframe
 print frames
 print "\n\n"
@@ -23,6 +34,58 @@ except:
     exit()
 
 import os
+import datetime
 for i in range(int(startframe), int(frames)+1):
+    
+    startframetime = datetime.datetime.now()
+    
+    
+    #### ACTUALL RENDER
+    os.system(blenderpath+" -b "+blendfile+ " -o "+destination+"#### -F "+extention+" -f "+str(i))
+    #### ENDS HERE
+    
+    
+    
+    endframetime = datetime.datetime.now()
+    DELTA = endframetime - startframetime
+    seconds = DELTA.seconds
+    
+    try:
+        readfile = open(blendfile[:blendfile.rfind("/")]+"/renderspeed.data", "r")
+        readfile = readfile.read()
+    except:
+        
+        readfile = ""
+    
+    print blendfile[:blendfile.rfind("/")]+"/renderspeed.data"
 
-    os.system(blenderpath+" -b "+blendfile+" -f "+str(i))
+    read = []
+    for r in readfile[:-1].split("\n"):
+        read.append(r)
+    
+    readfile = read
+    
+    
+    foundframe = False
+    for index, line in enumerate(readfile):
+        if line.startswith(str(i)+" "):
+            foundframe = True
+            readfile[index] = str(i)+" "+str(seconds)
+    if foundframe == False:
+        readfile.append(str(i)+" "+str(seconds))
+    
+            
+    
+    
+    writefile = open(blendfile[:blendfile.rfind("/")]+"/renderspeed.data", "w")
+    for r in readfile:
+        writefile.write(r+"\n")
+    
+    writefile.close()
+    
+    
+    
+    
+    
+    
+    

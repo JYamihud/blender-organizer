b807c1b502c411132ffd9ab1dbf290b46a3ef099
JYamihud <blenderdumbass@gmail.com>
2018-09-11 14:08:20 +0300

    Update checklist.py

--- py_data/modules/checklist.py
+++ py_data/modules/checklist.py
@@ -23,6 +23,11 @@ import socket
 import gtk
 import pango
 import cairo
+import glib
+
+# self made modules
+import dialogs
+
 
 
 ### READ FILE ####
@@ -77,8 +82,8 @@ def partcalculate(part):
     else:
         for i in part[1:]:
             fraction = fraction + (partcalculate(i) / len(part[1:]))
-            print i
-            print fraction
+            #print i
+            #print fraction
     
     
     
@@ -91,7 +96,405 @@ def partcalculate(part):
 ### CHECKLIST MANAGER WINDOW ###
 # and finally we need a window editor for those graphs
 
-def checkwindow(w=False, title="Checklist"):
-    mainwin = gtk.Window()
-    
+class checkwindow:
+    def __init__(self, w=False, pf="/", title="Checklist", FILE=None):
+        
+        #saving all the input to SELF
+        self.widget = w
+        self.title = title
+        self.FILE = FILE
+        self.FILENAME = FILE
+        
+        self.LIST = openckecklist(self.FILE)
+        self.mainpercent = partcalculate(self.LIST)
+        
+        self.open()
+        
+        
+        self.pf = pf
+        
+        self.win = gtk.Window()
+        self.win.set_title(self.title+"   "+FILE.replace(self.pf, ""))
+        self.win.set_default_size(800,800)
+        self.win.set_position(gtk.WIN_POS_CENTER)
 
+        self.mainbox = gtk.VBox(False)
+        self.win.add(self.mainbox)
+        
+        self.allowed = True #allowed to refresh frame
+        
+        # HELPERS
+        
+        self.dW = 0
+        self.DH = 0
+        
+        self.mpx = 0
+        self.mpy = 0
+        self.mpf = 0
+          
+        self.offset = 0       
+        
+        ## ICONS
+        
+        self.ok = gtk.gdk.pixbuf_new_from_file(self.pf+"/py_data/icons/ok.png")
+        self.plus = gtk.gdk.pixbuf_new_from_file(self.pf+"/py_data/icons/plus.png")
+        self.delete = gtk.gdk.pixbuf_new_from_file(self.pf+"/py_data/icons/delete.png")
+        
+        
+        
+        graph = gtk.DrawingArea()
+        graph.set_size_request(500,700)
+        
+        self.mainbox.pack_start(graph)
+        graph.connect("expose-event", self.framegraph) 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        self.win.show_all()
+    
+    def open(self):
+        self.FILE = open(self.FILENAME, "r")
+        self.FILE = self.FILE.read().split("\n")
+        
+    def save(self):
+        
+        n = []
+        
+        for i in self.FILE:
+            
+            if i != "":  
+                
+                n.append(i)
+        self.FILE = n
+        
+        
+        save = open(self.FILENAME, "w")
+        
+        if self.FILE[-1] == "":
+            self.FILE = self.FILE[:-1]
+        
+        for i in self.FILE:
+        
+            save.write(i+"\n")
+        
+        save.close()
+    
+    
+    #### THIS FUNCTION DRAWS THE PIXELS IN THE WINDOW ####
+    def framegraph(self, widget, event):
+             
+        
+                                               
+        w, h = widget.window.get_size()
+        xgc = widget.window.new_gc()
+        
+        mx, my, fx  = widget.window.get_pointer()
+        
+        
+        # GETTING WHETHER THE WINDOW IS ACTIVE
+        
+        self.winactive = self.win.is_active()
+        
+        ctx = widget.window.cairo_create()
+        ctx.select_font_face("Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        
+        xgc.line_width = 2
+        
+        # BACKGROUND COLOR
+        xgc.set_rgb_fg_color(gtk.gdk.color_parse("#868686")) ## CHOSE COLOR
+        widget.window.draw_rectangle(xgc, True, 0, 0, w, h)  ## FILL FRAME  
+        
+        widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.ARROW))
+        #############################################################################
+        ############################# DRAW HERE #####################################
+        #############################################################################
+        
+        
+        
+        
+        
+        
+        
+        prevline = "[ ]"
+        sofar = [False, 0]
+        for ind, line in enumerate(self.FILE[9:]):
+            
+            
+                
+                
+                
+            
+            if "[ ]" in line or "[V]" in line or "[v]" in line:
+                
+                
+                if my in range(ind*40+self.offset, ind*40+self.offset+35):
+                    
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#7c7c7c")) ## CHOSE COLOR
+                    widget.window.draw_rectangle(xgc, True, line.find("[")*20, ind*40+self.offset,  w, 39)
+                
+                
+                
+                # IF IT ALREADY WAS CHECK OFF AT EARLIER STAGE
+                
+                if prevline.find("[") < line.find("["):
+                    
+                    if prevline[prevline.find("[")+1:].startswith("V") or prevline[prevline.find("[")+1:].startswith("v"): 
+                        sofar = [True, line.find("[")]
+                        
+                if sofar[1] > line.find("["):
+                    sofar = [False, line.find("[")]
+                
+                
+                ctx.set_source_rgb(1,1,1)
+                if sofar[0]:
+                    ctx.set_source_rgb(0.6,1,0.6)
+                ctx.set_font_size(20)
+                ctx.move_to(  line.find("[")*20+30, ind*40+25+self.offset)
+                ctx.show_text(line[line.find("]")+1:])
+                
+                
+                
+                # CHECK BUTTON
+                
+                
+                
+                
+                xgc.set_rgb_fg_color(gtk.gdk.color_parse("#5c5c5c")) ## CHOSE COLOR
+                # IF MOUSE OVER
+                if my in range(ind*40+5+self.offset, ind*40+5+self.offset+20) and mx in range(line.find("[")*20+5, line.find("[")*20+5+20):
+                    widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#e47649"))
+                    
+                    # IF CLICKED
+                    if "GDK_BUTTON1" in str(fx) and "GDK_BUTTON1" not in str(self.mpf) and self.win.is_active():
+                        
+                        put = "V"
+                        if line[line.find("[")+1:].startswith("V") or line[line.find("[")+1:].startswith("v"):
+                            put = " "
+                            
+                        
+                         
+                        self.FILE[ind+9] = line[:line.find("[")+1]+put+line[line.find("]"):]
+                        
+                        # refrashing the file
+                        self.save()
+                        self.open()
+                    
+                    
+                    
+                widget.window.draw_rectangle(xgc, True, line.find("[")*20+5, ind*40+5+self.offset, 20, 20)
+                
+                
+                if line[line.find("[")+1:].startswith("V") or line[line.find("[")+1:].startswith("v"):
+                    
+                    widget.window.draw_pixbuf(None, self.ok, 0, 0, line.find("[")*20+7, ind*40+self.offset , -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
+                
+                
+                # ADD SUBTASK
+                
+                
+                if my in range(ind*40+5+self.offset, ind*40+5+self.offset+20) and mx in range(line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+20):
+                    widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#e47649"))
+                    widget.window.draw_rectangle(xgc, True, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, ind*40+5+self.offset-2, 22, 22)
+                    
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#5c5c5c"))
+                    widget.window.draw_rectangle(xgc, True, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+30, ind*40+5+self.offset-2, 160, 30)
+                    
+                    
+                    ctx.set_source_rgb(1,1,1)
+                    ctx.set_font_size(20)
+                    ctx.move_to(  line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+35, ind*40+25+self.offset)
+                    ctx.show_text("Add Subtask")
+                    
+                    
+                    
+                    # IF CLICKED
+                    if "GDK_BUTTON1" in str(fx) and "GDK_BUTTON1" not in str(self.mpf) and self.win.is_active():
+                        
+                        def ee(theline, line):
+                                
+                            Pname = ""
+                            Pname = dialogs.PickName("New Subtask")
+                            
+                            
+                            
+                            
+                            if Pname != "":
+                                self.FILE.insert(theline+10,  line[:line.find("[")]+"    [ ] "+Pname)
+                                
+                                
+                                # refrashing the file
+                                self.save()
+                                self.open()
+                            
+                            
+                        
+                        
+                        glib.timeout_add(10, ee, ind, line)
+                
+                
+                
+                
+                
+                widget.window.draw_pixbuf(None, self.plus, 0, 0, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, ind*40+self.offset+5 , -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
+                    
+                
+                
+                
+                # DELETE TASK
+                
+                
+                
+                # IF MOUSE OVER
+                if my in range(ind*40+5+self.offset, ind*40+5+self.offset+20) and mx in range(w-40, w-40+20):
+                    widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#e47649"))
+                    widget.window.draw_rectangle(xgc, True, w-42, ind*40+5+self.offset-2, 22, 22)
+                    
+                    xgc.set_rgb_fg_color(gtk.gdk.color_parse("#5c5c5c"))
+                    widget.window.draw_rectangle(xgc, True, w-210, ind*40+5+self.offset-2, 160, 30)
+                    
+                    
+                    ctx.set_source_rgb(1,1,1)
+                    ctx.set_font_size(20)
+                    ctx.move_to(  w-200, ind*40+25+self.offset)
+                    ctx.show_text("Delete Task")
+                    
+                    
+                    
+                    # IF CLICKED
+                    if "GDK_BUTTON1" in str(fx) and "GDK_BUTTON1" not in str(self.mpf) and self.win.is_active():
+                        
+                        del self.FILE[ind+9]
+                
+                        # refrashing the file
+                        self.save()
+                        self.open()
+                        
+                
+                widget.window.draw_pixbuf(None, self.delete, 0, 0, w-40, ind*40+self.offset+5 , -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
+                
+                
+                
+            
+                
+                
+            prevline = line
+        
+            
+            
+            
+            
+            
+        # ADD SUBTASK
+                
+                
+        if my in range(ind*40+5+self.offset, ind*40+5+self.offset+20) and mx in range(line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+20):
+            widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.HAND1))
+            xgc.set_rgb_fg_color(gtk.gdk.color_parse("#e47649"))
+            widget.window.draw_rectangle(xgc, True, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, ind*40+5+self.offset-2, 22, 22)
+            
+            xgc.set_rgb_fg_color(gtk.gdk.color_parse("#5c5c5c"))
+            widget.window.draw_rectangle(xgc, True, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+30, ind*40+5+self.offset-2, 160, 30)
+            
+            
+            ctx.set_source_rgb(1,1,1)
+            ctx.set_font_size(20)
+            ctx.move_to(  line.find("[")*20+(len(line[line.find("]")+1:])*12)+35+35, ind*40+25+self.offset)
+            ctx.show_text("Add Task")
+            
+            
+            
+            # IF CLICKED
+            if "GDK_BUTTON1" in str(fx) and "GDK_BUTTON1" not in str(self.mpf) and self.win.is_active():
+                
+                def ee(theline, line):
+                        
+                    Pname = ""
+                    Pname = dialogs.PickName("New Task")
+                    
+                    
+                    
+                    
+                    if Pname != "":
+                        self.FILE.append("[ ] "+Pname)
+                        
+                        
+                        # refrashing the file
+                        self.save()
+                        self.open()
+                    
+                    
+                
+                
+                glib.timeout_add(10, ee, ind, line)
+        
+        
+        
+        
+        
+        widget.window.draw_pixbuf(None, self.plus, 0, 0, line.find("[")*20+(len(line[line.find("]")+1:])*12)+35, ind*40+self.offset+5 , -1, -1, gtk.gdk.RGB_DITHER_NONE, 0, 0)
+        
+        
+        
+        
+        
+        #SCROLL
+        
+        if self.mpy > my and "GDK_BUTTON2" in str(fx) and "GDK_BUTTON2" in str(self.mpf) and self.win.is_active():
+                    
+            self.offset = self.offset + (my-self.mpy)
+        
+        if self.mpy < my and "GDK_BUTTON2" in str(fx) and "GDK_BUTTON2" in str(self.mpf) and self.win.is_active():
+                    
+            self.offset = self.offset - (self.mpy-my)
+        
+        
+        if self.offset < 0-(ind*40+40-h):
+            self.offset = 0-(ind*40+40-h)
+            
+        if self.offset > 0:
+            self.offset = 0
+        
+        
+        
+        
+        
+        #############################################################################
+        ############################# UNTIL HERE ####################################
+        #############################################################################
+        
+        
+        
+        # TESTING SOMETHING
+        ctx.set_font_size(20)
+        ctx.move_to( mx, my)
+        #ctx.show_text(str(mx)+":"+str(my)+"  "+str(self.mainscroll)) 
+        
+        self.dW = w
+        self.DH = h
+        
+        self.mpx = mx
+        self.mpy = my
+        self.mpf = fx
+        
+        
+        
+        def callback():
+            if self.allowed == True:
+                widget.queue_draw()
+
+        glib.timeout_add(10, callback)
+    

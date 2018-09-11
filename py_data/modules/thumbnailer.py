897e94c15a19e1570b6362d9f282644bf2819d18
JYamihud <blenderdumbass@gmail.com>
2018-09-11 14:09:28 +0300

    Update thumbnailer.py

--- py_data/modules/thumbnailer.py
+++ py_data/modules/thumbnailer.py
@@ -3,13 +3,17 @@ try:
 except:
     from PIL import Image
 import os
-def thumbnail(im, x=200, y=200):
+def thumbnail(im, x=200, y=200, pf=False):
     
     thrumb = Image.open(im)
     size = x, y
     thrumb.thumbnail(size, Image.NEAREST)
-    thrumb.save("py_data/tmp.png", "PNG")
-    return(os.getcwd()+"/py_data/tmp.png")
+    if not pf:
+        thrumb.save("py_data/tmp.png", "PNG")
+        return(os.getcwd()+"/py_data/tmp.png")
+    else:
+        thrumb.save(pf+"/py_data/tmp.png", "PNG")
+        return(pf+"/py_data/tmp.png")
     
 def videothumb(im, s=200, y=200):
     os.system("totem-video-thumbnailer -s "+str(s)+" "+im+" "+os.getcwd()+"/py_data/tmp.png")
@@ -30,6 +34,5 @@ def blenderthumb(im, x=200, y=200):
     
     
     return(os.getcwd()+"/py_data/tmp.png") 
-
 
 
